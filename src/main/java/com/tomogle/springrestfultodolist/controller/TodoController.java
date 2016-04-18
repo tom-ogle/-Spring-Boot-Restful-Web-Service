15504c8c37990898f7902ede907b75671affb40b
tom-ogle <t.ogle.uk@gmail.com>
2016-04-18 20:24:21 +0100

    Basic working quickstart restful app skeleton

---
+++ src/main/java/com/tomogle/springrestfultodolist/controller/TodoController.java
@@ -0,0 +1,20 @@
+package com.tomogle.springrestfultodolist.controller;
+
+import com.tomogle.springrestfultodolist.domain.Todo;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ */
+@RestController
+@RequestMapping("/todo")
+public class TodoController {
+
+  @RequestMapping(method = RequestMethod.GET)
+  public Todo getTodoById(@RequestParam(value="id") long id) {
+    return new Todo(id, "Hard coded Todo", "Some contents");
+  }
+}
---
+++ src/main/java/com/tomogle/springrestfultodolist/domain/Todo.java
@@ -0,0 +1,29 @@
+package com.tomogle.springrestfultodolist.domain;
+
+/**
+ *
+ */
+public class Todo {
+
+  private final long id;
+  private final String title;
+  private final String content;
+
+  public Todo(long id, String title, String content) {
+    this.id = id;
+    this.title = title;
+    this.content = content;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public String getContent() {
+    return content;
+  }
+}
